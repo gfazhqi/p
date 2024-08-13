5a28396bd39b85799187739b5dd4a0b51c1787ae
gfazhqi <163487130+gfazhqi@users.noreply.github.com>
2024-08-13 22:53:02 +0700

    Update otp.py

--- otp.py
+++ otp.py
@@ -1,70 +1,29 @@
-import logging
-import os
-import sys
+from pyrogram import Client
 import asyncio
-from pyrogram import Client, filters
-from pyrogram.errors import FloodWait, PhoneNumberBanned, PhoneCodeInvalid, SessionPasswordNeeded
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.WARN)
-logger = logging.getLogger(__name__)
+api_id = 13875777
+api_hash = '28bac7e8ca985a86f48aadc28b1b3916'
+sesi = 'nama_sesi'
 
-api_id = int(os.getenv('13875777'))
-api_hash = os.getenv('28bac7e8ca985a86f48aadc28b1b3916')
-
-async def start_client(phone_number):
-    client = Client(f'session/{phone_number}', api_id, api_hash, phone_number=phone_number)
-    await client.connect()
-    return client
-
-async def fetch_account_info(client, phone_number):
-    try:
-        me = await client.get_me()
-        logger.info(f'TELECODE RECEIVER: Un: {me.username}, No: {phone_number} ▪︎ {me.first_name}')
-        logger.info('Waiting to receive code...\n')
-    except Exception as e:
-        logger.error(f'Error fetching account info: {e}')
+app = Client(name=sesi, api_id=api_id, api_hash=api_hash, workdir="sessions/")
 
 async def main():
-    if len(sys.argv) < 2:
-        print('Usage: python start.py phone_number')
-        exit(1)
-    phone_number = sys.argv[1]
+    await app.start()
+    me = await app.get_me()
+    print(me)
+    await app.stop()
 
-    if not os.path.exists("session"):
-        os.mkdir("session")
+async def get_otp(sesi):
+    async with Client(name=sesi, api_id=api_id, api_hash=api_hash, workdir='sessions/') as app:
+        await asyncio.sleep(3)
+        async for message in app.get_chat_history(chat_id=777000, limit=5):
+            pesan = message.text
+            angka = ''.join([char for char in pesan if char.isdigit()])
+            print(pesan)
+            return angka
 
-    try:
-        client = await start_client(phone_number)
-        await client.start()
+# Run the main function to test getting account information
+asyncio.run(main())
 
-        if not client.is_authorized:
-            try:
-                await client.send_code(phone_number)
-                await client.sign_in(phone_number, input('Enter the code: '))
-            except SessionPasswordNeeded:
-                logger.error(f'{phone_number} Session Password Needed!')
-                await client.stop()
-            except PhoneCodeInvalid:
-                logger.error(f'{phone_number} ▪︎ Code Invalid!')
-                await client.stop()
-
-        await fetch_account_info(client, phone_number)
-
-        @client.on_message(filters.private & filters.user(777000))
-        async def handler(client, message):
-            if message.text:
-                logger.info(message.text)
-
-        logger.info('[!] Connected!')
-        await client.idle()
-    except PhoneNumberBanned:
-        logger.error(f'{phone_number} ▪︎ Number Banned!')
-        sys.exit()
-    except FloodWait as e:
-        logger.error(f'{phone_number} ▪︎ Flood Wait Error! {e.x} seconds')
-        sys.exit()
-    except PhoneCodeInvalid:
-        logger.error(f'{phone_number} ▪︎ Code Invalid!')
-
-if __name__ == '__main__':
-    asyncio.run(main())
+# Run the get_otp function to test getting OTP messages
+asyncio.run(get_otp(sesi))
